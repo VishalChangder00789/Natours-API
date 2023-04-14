599a51c3628cc615d24871f8ef7906eefc38a3d4
Bishal Changder <changdervishal1999@gmail.com>
2023-04-14 14:47:11 +0530

    Seperated server and app file

--- app.js
+++ app.js
@@ -1,124 +1,55 @@
-const { Console } = require("console");
 const express = require("express");
-const fs = require("fs");
 const app = express();
-
-//Todo : variables
-const port = 3000;
-const filePath = `${__dirname}/dev-data/data/tours-simple.json`;
-
-//Todo: Read Data
-//?? This statement will take all the data from tours-simple.json file in a json format
-//?? Then convert it to Javascript object and store in tours variable.
-
-const tours = JSON.parse(fs.readFileSync(filePath));
+const morgan = require("morgan");
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
 
 //! Middlewares
+// It will be ran before req and res cycle ends
+app.use(morgan("dev")); // Logging POST/api/v1/users 500 11.472 ms -58 MESSAGES LIKE THIS IS MORGAN
 
-// It will be ran before req and res cycles
-
-//#region --------- Middleware (Recognize incoming req object has JSON object) ---------
+// Third party middleware
+//Middleware (Recognize incoming req object has JSON object)
 app.use(express.json());
 
-//#endregion --------- Middleware (Recognize incoming req object has JSON object) ---------
-
-/*
-
-
-
-///////////////////////// SEPERATION  /////////////////////////
-
-
-*/
-
-//! Basic routing
-
-//#region --------- BASIC ROUTING ---------
-// app.get("/", (req, res) => {
-//   res.status(200).json({
-//     message: "recieving from server",
-//     app: "Natours :  Bishal Changder",
-//   });
-// });
-//#endregion --------- BASIC ROUTING ---------
-
-//#region ---------- GET ALL TOURS ----------
-app.get("/api/v1/tours", (req, res) => {
-  res.status(200).json({
-    status: "success",
-    results: tours.length,
-    data: {
-      tours: tours,
-    },
-  });
-});
-//#endregion ---------- GET ALL TOURS ----------
-
-//#region ---------- GET A TOURS ----------
-app.get("/api/v1/tours/:id", (req, res) => {
-  const _param_id = req.params.id * 1;
-
-  if (_param_id > tours.length) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid Id",
-    });
-  }
-
-  const newTour = tours.find((el) => el.id === _param_id);
-
-  res.status(200).json({
-    status: "success",
-    data: {
-      tours: newTour,
-    },
-  });
-});
-//#endregion ---------- GET ALL TOURS ----------
-
-/*
-
-
-
-///////////////////////// SEPERATION  /////////////////////////
-
-
-*/
-//! Posting Data
-//#region --------- POST A TOUR ---------
-app.post(`/api/v1/tours`, (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-  fs.writeFile(filePath, JSON.stringify(tours), (err) => {
-    res.status(201).json({
-      status: "success",
-      data: {
-        tour: newTour,
-      },
-    });
-  });
+// Making own  custom middleware
+app.use((req, res, next) => {
+  console.log("Running");
+  req.requestTime = new Date().toISOString();
+  next(); // if not called req and res cycle won't end
 });
 
-//#endregion --------- POST A TOUR ---------
+// Routing Middleware
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
 
-//#region --------- BASIC POSTING ---------
+//#region --------------------------- OLD ROUTING WAY ---------------------------
+// ! Still Complex : OLD WAY
+// ! having same routes
+// app.get("/api/v1/tours", getAllTours);
+// app.post(`/api/v1/tours`, postTour);
+// !having same routes
+// app.get("/api/v1/tours/:id", getTour);
+// app.patch("/api/v1/tours/:id", updateTour);
+// app.delete("/api/v1/tours/:id", deleteTour);
 
-app.post("/", (req, res) => {
-  res.send("Endpoint active");
-});
-//#endregion --------- BASIC POSTING ---------
+// // Todo simplified : NEW WAY
+// //! Tour route
+// app.route("/api/v1/tours").get(getAllTours).post(postTour);
+// app
+//   .route("/api/v1/tours/:id")
+//   .get(getTour)
+//   .patch(updateTour)
+//   .delete(deleteTour);
 
-/*
+// //! User routes
+// app.route("/api/v1/users").get(getAllUsers).post(postUser);
+// app
+//   .route("/api/v1/users/:id")
+//   .get(getUser)
+//   .patch(updateUser)
+//   .delete(deleteUser);
 
+//#endregion --------------------------- OLD ROUTING WAY ---------------------------
 
-
-///////////////////////// SEPERATION  /////////////////////////
-
-
-*/
-// ? Starting server
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-});
+module.exports = app;
