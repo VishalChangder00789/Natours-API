69a527471818ad7d5977942037b0e7a3180855e3
Bishal Changder <changdervishal1999@gmail.com>
2023-05-25 14:28:38 +0530

    Schema for Users

---
+++ model/userModel.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter a name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter an email"],
+    unique: true,
+    validate: [validator.isEmail, "please provide a valid email"],
+    lowercase: true,
+  },
+  photo: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Please confirm a password"],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: "Passwords are not the same",
+    },
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
